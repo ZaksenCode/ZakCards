e8d68f6551607b3fdbdf11adedf751bbf57e717f
ZaksenCode <maksimsidorkin2006@mail.ru>
2024-09-22 15:48:50 +0300

    intial commit

---
+++ ZakCards.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BepInEx;
+using UnboundLib.Cards;
+using ZakCards.scripts;
+
+namespace ZakCards {
+    [BepInDependency("com.willis.rounds.unbound")]
+    [BepInDependency("pykess.rounds.plugins.moddingutils")]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch")]
+    [BepInPlugin("zakcards", "ZakCards", "1.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class ZakCards : BaseUnityPlugin
+    {
+        internal static string modInitials = "ZK";
+        internal static AssetBundle assets;
+
+        void Awake() {
+            assets = Jotunn.Utils.AssetUtils.LoadAssetBundleFromResources("zakcardsassets", typeof(ZakCards).Assembly);
+            assets.LoadAsset<GameObject>("ModCards").GetComponent<CardHolder>().RegisterCards();
+        }
+
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
---
+++ scripts/CardHolder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnboundLib.Cards;
+
+namespace ZakCards.scripts {
+    public class CardHolder: MonoBehaviour {
+        public List<GameObject> Cards;
+        public List<GameObject> HiddenCards;
+
+        internal void RegisterCards(){
+            foreach(var Card in Cards){
+                CustomCard.RegisterUnityCard(Card, ZakCards.modInitials, Card.GetComponent<CardInfo>().cardName, true, null);
+            }
+            foreach(var Card in HiddenCards){
+                CustomCard.RegisterUnityCard(Card, ZakCards.modInitials, Card.GetComponent<CardInfo>().cardName, false, null);
+                ModdingUtils.Utils.Cards.instance.AddHiddenCard(Card.GetComponent<CardInfo>());
+            }
+        }
+    }
+}
